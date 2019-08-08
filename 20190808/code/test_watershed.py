8e9679212922b2ab6c3340ca79b241cad2eedc3f
3211187 <32090903+3211187@users.noreply.github.com>
2019-08-08 14:55:16 +0900

    Update test_watershed.py

--- 20190808/code/test_watershed.py
+++ 20190808/code/test_watershed.py
@@ -8,14 +8,33 @@ from PIL import Image
 from PIL import ImageTk
 import tkinter.filedialog as filedialog
 
+x_start, y_start, x_end, y_end = 0, 0, 0, 0
+
+def crop_sub():
+    global cropping
+    global panelB
+    if panelB != None:
+        cropping = True
+        select_image()
+
+def select_sub():
+    global cropping
+    global path_img
+    path_img = None
+    cropping = False
+    select_image()
 
 
 def save_seg_image():
     global pil_result
-    file = filedialog.asksaveasfile(mode='w', defaultextension=".jpg")
-    if file:
-        # pil_img = Image.fromarray(pil_result)
-        pil_result.save(file)  # saves the image to the input file name.
+    global panelB
+    if panelB != None:
+        file = filedialog.asksaveasfile(mode='w', defaultextension=".jpg")
+        if file:
+            pil_img = Image.fromarray(pil_result)
+            #cv2.imshow("img",pil_result)
+            pil_img.save(file)  # saves the image to the input file name.
+
 
 
 def select_image():
@@ -23,31 +42,33 @@ def select_image():
     global panelA, panelB
     global pil_result
     global path_img
+    global oriImage
+
 
     # open a file chooser dialog and allow the user to select an input
     # image
-    path = filedialog.askopenfilename()
-    path_img  = path
+    if path_img == None :
+        path = filedialog.askopenfilename()
+        path_img  = path
 
     # ensure a file path was selected
-    if len(path) > 0:
+    if len(path_img) > 0:
         # load the image from disk, convert it to grayscale, and detect
         # edges in it
-
-        img = cv.imread(path)
-
-
-        pil_img = Image.fromarray(img)
-        # pil_result is used for saving
-        pil_result = pil_img
-
         oriImage = cv.imread(path_img)
-        cv2.rectangle(oriImage, (x_start, y_start), (x_end, y_end), (255, 0, 0), 2)
-        cv2.imshow("image", oriImage)
-        cv2.namedWindow("image")
-        cv2.setMouseCallback("image", mouse_crop)
 
-        pil_img = pil_result
+        if cropping:
+
+            #cv2.rectangle(oriImage, (x_start, y_start), (x_end, y_end), (255, 0, 0), 2)
+            cv2.imshow("image", oriImage)
+            cv2.namedWindow("image")
+            cv2.setMouseCallback("image", mouse_crop)
+        else:
+            print(cropping)
+            #oriImage = cv.imread(path)
+            pil_result = oriImage
+
+        pil_img = Image.fromarray(pil_result)
 
         #pil_gray = Image.fromarray(gray)
 
@@ -56,24 +77,6 @@ def select_image():
         edged = ImageTk.PhotoImage(pil_img)
 
         # if the panels are None, initialize them
-        '''
-        if panelA is None or panelB is None:
-           # the first panel will store our original image
-           panelA = Label(image=image)
-           panelA.image = image
-           panelA.pack(side="left", padx=10, pady=10)
-           # while the second panel will store the edge map
-           panelB = Label(image=edged)
-           panelB.image = edged
-           panelB.pack(side="right", padx=10, pady=10)
-        # otherwise, update the image panels
-        else:
-        # update the pannels
-           panelA.configure(image=image)
-           panelB.configure(image=edged)
-           panelA.image = image
-           panelB.image = edged
-        '''
 
         if panelB is None:
             # while the second panel will store the edge map
@@ -87,12 +90,11 @@ def select_image():
             panelB.image = edged
 
 
-cropping = False
-x_start, y_start, x_end, y_end = 0, 0, 0, 0
+
 
 
 def mouse_crop(event, x, y, flags, param):
-
+    global panelB
     global pil_result
     # grab references to the global variables
     global x_start, y_start, x_end, y_end, cropping
@@ -122,27 +124,12 @@ def mouse_crop(event, x, y, flags, param):
             #cv2.imshow("Cropped", roi)
             pil_result = roi
 
+            pil_img = Image.fromarray(pil_result)
 
-def crop_sub():
-    global oriImage
-    global panelC
-    print(path_img)
-
-    oriImage = cv.imread(path_img)
-    cv2.rectangle(oriImage, (x_start, y_start), (x_end, y_end), (255, 0, 0), 2)
-    cv2.imshow("image", oriImage)
-    cv2.namedWindow("image")
-    cv2.setMouseCallback("image", mouse_crop)
-
-
-
-    pil_img = pil_result
-
-    # ...and then to ImageTk format
-    #image = ImageTk.PhotoImage(pil_gray)
-    edged = ImageTk.PhotoImage(pil_img)
-
-
+            edged = ImageTk.PhotoImage(pil_img)
+            # update the pannels
+            panelB.configure(image=edged)
+            panelB.image = edged
 
 
 
@@ -152,12 +139,13 @@ def crop_sub():
 root = Tk()
 panelA = None
 panelB = None
+path_img = None
 pil_result = None
 
 # create a button, then when pressed, will trigger a file chooser
 # dialog and allow the user to select an input image; then add the
 # button the GUI
-btn = Button(root, text="Select an image", command=select_image)
+btn = Button(root, text="Select an image", command=select_sub)
 btn.pack(side="top", anchor = "center",expand="yes", padx="10", pady="10")
 btn = Button(root, text="Crop an image", command=crop_sub)
 btn.pack(side="top", anchor = "center",expand="yes", padx="10", pady="10")
